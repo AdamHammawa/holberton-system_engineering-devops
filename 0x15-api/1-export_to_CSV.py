6745af92f447a914e88bd737e907511bef01590d
mikesosa <michael.sosa@holbertonschool.com>
2020-02-03 16:43:33 -0500

    Pep9

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,10 +10,12 @@ if __name__ == "__main__":
     # Making sure the argument passed is an int
     employee_id = int(argv[1])
     employee_info = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)).json()
+        "https://jsonplaceholder.typicode.com/users/{}"
+        .format(employee_id)).json()
     employee_name = employee_info['name']
     employee_todosList = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)).json()
+        'https://jsonplaceholder.typicode.com/todos?userId={}'
+        .format(employee_id)).json()
     # writers a csv file now with #.csv as the file name
     with open("{}.csv".format(employee_id), "w") as f:
         row = csv.writer(f, delimiter=",", quoting=csv.QUOTE_ALL)
