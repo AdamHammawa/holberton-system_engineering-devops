31d4bfb8eb71851294283f6d5cb3fc484634c884
mikesosa <michael.sosa@holbertonschool.com>
2020-02-03 16:42:33 -0500

    Pep8

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,15 +11,17 @@ if __name__ == "__main__":
     # Making sure the argument passed is an int
     employee_id = int(argv[1])
     employee_info = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)).json()
+        "https://jsonplaceholder.typicode.com/users/{}"
+        .format(employee_id)).json()
     employee_name = employee_info['name']
     employee_todosList = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id)).json()
+        'https://jsonplaceholder.typicode.com/todos?userId={}'
+        .format(employee_id)).json()
     for todo in employee_todosList:
         todos_List.append(todo.get("completed"))
         if (todo.get("completed")):
             todos_Completed.append(todo.get("title"))
-    print("Eployee {} is done with tasks({}/{}):".format(employee_name,
-                                                         len(todos_Completed), len(todos_List)))
+    print("Eployee {} is done with tasks({}/{}):"
+          .format(employee_name, len(todos_Completed), len(todos_List)))
     for task in todos_Completed:
         print("\t {}".format(task))
