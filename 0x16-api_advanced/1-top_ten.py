757512aabe98028eb3c41b8bf39c8126fd7abe29
mikesosa <michael.sosa@holbertonschool.com>
2020-02-06 16:31:57 -0500

    Top 10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Write a function that queries the Reddit API and returns the number of
-subscribers (not active users, total subscribers) for a given subreddit. If an
-invalid subreddit is given, the function should return 0.
+Write a function that queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit.
 """
 
 
@@ -10,17 +9,15 @@ def top_ten(subreddit):
     """ does what is stated above """
     import requests
 
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=9'.format(subreddit)
-    request = requests.get(url,
-                           headers={'User-Agent': 'Mike'},
-                           allow_redirects=False)
+    url = "https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit)
+    request = requests.get(url, headers={'User-Agent': 'Byn'})
     if request.status_code != 200:
-        print (None)
+        print(None)
         return None
     request = request.json()
     if 'data' in request:
-        for post in request.get('data').get('children'):
-            print(post.get('data').get('title'))
+        for thread in request.get('data').get('children'):
+            print(thread.get('data').get('title'))
     else:
         print(None)
         return None
